9091d4d8ce501bd4861211b728330bfd212d3966
Nguyen Van Tuan <85727520+tuannvhust@users.noreply.github.com>
2022-03-11 20:39:13 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import utils 
+import scenario
+
+def main():
+    args = utils.get_args()
+    method = getattr(scenario, args.scenario)
+    method(args)
+
+if __name__ == '__main__':
+    main()
